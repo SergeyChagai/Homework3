d9bcaef56f9a3413de8f88097368a614abfacd92
Serjo <Serjo@DESKTOP-TRAVELM>
2020-10-21 01:20:56 +0300

    Комментарии

--- Homework3/9_sort/Program.cs
+++ Homework3/9_sort/Program.cs
@@ -8,32 +8,29 @@ namespace _9_sort_increase
         {
             while (true)
             {
-                //ввод чисел массива
                 Console.WriteLine("Введите количество элементов массива:");
                 int n = Convert.ToInt32(Console.ReadLine());
-                //Console.WriteLine($"Введите {n} чисел:");
-                int[] numbers = new int[n];
-                //for (int number = 0; number < numbers.Length; number++)
-                //    numbers[number] = Convert.ToInt32(Console.ReadLine());
-                Random r = new Random();
-                for (int i = 0; i < numbers.Length; i++)
+                int[] numbers = new int[n]; //создаем массив типа int[] с количеством элементов равным n
+                Random r = new Random();    //создаем новую переменную класса Random
+                
+                for (int i = 0; i < numbers.Length; i++)    //заполняем массив | numbers.Length - длина массива (то же самое, что и n, в данном случае)
                 {
-                    numbers[i] = r.Next(-100, 100);
-                    Console.Write(numbers[i] + ", ");
+                    numbers[i] = r.Next(-100, 100);         //r.Next() выдает рандомное число типа int | аргументы (то, что в скобках - диапазон,
+                    Console.Write(numbers[i] + ", ");       //из которого будут выбираться рандомные числа) | вывод полученного массива в консоль
                 }
 
                 //перебор массива выбором
-                for (int i = 0; i < numbers.Length; i++)
+                for (int i = 0; i < numbers.Length; i++)        //внешний цикл дает указание двигаться вправо, когда предыдущий элемент встал на свое место
                 {
-                    int max = i;
-                    for (int j = i; j < numbers.Length; j++)
+                    int max = i;                                //в переменную max сохраняем индекс (указание, какой по счету это элемент в массиве) максимального числа, пока что это будет i, то есть число, на котором остановился в данный момент внешний цикл
+                    for (int j = i; j < numbers.Length; j++)    //внутренний массив ищет максимальное число из еще неотсортированных чисел (поэтому сначала j = i, т.к. это первое неотсортированное число)
                     {
-                        if (numbers[j] > numbers[max])
+                        if (numbers[j] > numbers[max])          //если выполняется, что какое-то число больше, чем то, которое сейчас записано как максимальное, записываем индекс этого числа
                             max = j;
                     }
-                    int temp = numbers[i];
-                    numbers[i] = numbers[max];
-                    numbers[max] = temp;
+                    int temp = numbers[i];                      //после того, как нашли максимальное число из неотсортированных, меняем местами это максимальное число и то,
+                    numbers[i] = numbers[max];                  //что было в начале неотсортированного участка, после чего наш массив становится на 1 элемент более упорядоченным
+                    numbers[max] = temp;                        
                 }
 
                 //вывод результата
