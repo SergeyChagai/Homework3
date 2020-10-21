551305b598ffe38a249933d9963e7ca5a742ef1b
Serjo <Serjo@DESKTOP-TRAVELM>
2020-10-22 01:14:38 +0300

    набросок

--- Homework3(2D_arrays)/6_arr_reflection/Program.cs
+++ Homework3(2D_arrays)/6_arr_reflection/Program.cs
@@ -9,9 +9,9 @@ namespace _6_arr_reflection
             while (true)
             {
                 Console.Write("Введите количество строк таблицы: ");
-                int c = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите количество элементов в строках таблицы: ");
                 int s = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите количество элементов в строках таблицы: ");
+                int c = Convert.ToInt32(Console.ReadLine());
 
                 if (c == 1 || s == 1)
                 {
@@ -21,9 +21,11 @@ namespace _6_arr_reflection
                 int[,] arr = new int[s, c];
                 Random r = new Random();
 
-                for (int i = 0; i < arr.GetLength(1); i++)
+                int x = arr.GetLength(1);
+                int y = arr.GetLength(0);
+                for (int i = 0; i < y; i++)
                 {
-                    for (int j = 0; j < arr.GetLength(0); j++)
+                    for (int j = 0; j < x; j++)
                     {
                         arr[i, j] = r.Next(-100, 100);
                         if ((arr[i, j] > 0 && arr[i, j] / 10 != 0) || (arr[i, j] < 0 && ((arr[i, j] * -1) / 10) == 0))   //для более-менее красивого вывода
@@ -35,20 +37,57 @@ namespace _6_arr_reflection
                     Console.WriteLine("\n");
                 }
 
-                for (int i = 0; i < arr.GetLength(1); i++)
+                if (x != y)
                 {
-                    for (int j = i + 1; j < arr.GetLength(0); j++)
+                    if (y % 2 == 0)
                     {
-                        int temp = arr[i, j];
-                        arr[i, j] = arr[j, i];
-                        arr[j, i] = temp;
+                        for (int i = 0; i < y / 2; i++)
+                        {
+                            for (int j = 0; j < x; j++)
+                            {
+                                if ((j == 0 && i == 0) || (j == x - 1 && i == y - 1))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    int temp = arr[i, j];
+                                    arr[i, j] = arr[y - i - 1, x - j - 1];
+                                    arr[y - i - 1, x - j - 1] = temp;
+                                }
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < y / 2 + 1; i++)
+                        {
+                            for (int j = x / y + i; j < x; j++)
+                            {
+                                if ((j == 0 && i == 0) || (j == x - 1 && i == y - 1))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    int temp = arr[i, j];
+                                    arr[i, j] = arr[y - i - 1, x - j - 1];
+                                    arr[y - i - 1, x - j - 1] = temp;
+                                }
+                            }
+
+                        }
                     }
                 }
-
-                Console.WriteLine("----------------------------------------------\n");
-                for (int i = 0; i < arr.GetLength(1); i++)
+                else
                 {
-                    for (int j = 0; j < arr.GetLength(0); j++)
+
+                }
+                Console.WriteLine("----------------------------------------------\n");
+                for (int i = 0; i < y; i++)
+                {
+                    for (int j = 0; j < x; j++)
                     {
                         if ((arr[i, j] > 0 && arr[i, j] / 10 != 0) || (arr[i, j] < 0 && ((arr[i, j] * -1) / 10) == 0))   //для более-менее красивого вывода
                             Console.Write(" ");
